e4514b23265586c7aab1ff17e2dd2b235f08ce62
borovskayana <109426911+borovskayana@users.noreply.github.com>
2023-01-25 07:17:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 == 1) {
+    return false;
+  }
+  var stack = new Array();
+  for (var i = 0; i <= str.length - 1; i++) {
+    var find = false;
+    for (var n = 0; n < bracketsConfig.length; n++) {
+      if (str[i] == bracketsConfig[n][0]) {
+        find = true;
+        if (bracketsConfig[n][0] == bracketsConfig[n][1]) {
+          if (stack[stack.length - 1] == bracketsConfig[n][0]) {
+            stack.pop();
+            break;
+          }
+        }
+        stack.push(bracketsConfig[n][1]);
+        break;
+      }
+    }
+    if (!find) {
+      if (stack.length == 0 || str[i] != stack[stack.length - 1]) {
+        return false;
+      }
+      stack.pop();
+    }
+  }
+  return stack.length == 0 ? true : false;
+};
